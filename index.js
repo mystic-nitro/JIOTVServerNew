3dc57f9c77bf3f9959cf78acc7f7cac7e0e369e6
= <=>
2023-10-04 23:15:40 +0530

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import http from "http";
 import cors from "cors";
 import os from "node:os";
 import 'dotenv/config';
@@ -7,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 import fs from "fs";
 app.use(express.json());
-const PORT = 3500;
+const PORT = process.env.PORT || 3500;
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
@@ -19,6 +20,16 @@ const __dirname = path.dirname(__filename);
 // let networkInterfaces = os.networkInterfaces();
 // let ip = networkInterfaces["eth0"][0]["address"];
 // fs["writeFileSync"]("./ipData.jiotv", ip);
+
+http.createServer(app).listen(PORT, () => {
+  console.log(
+    "==================================================================="
+  );
+  console.log(
+    chalk.green("THIS SERVER IS 100% FREE. PLEASE DON'T PAY ANYONE.")
+  );
+})
+console.info(`Listening on port ${PORT}`);
 
 if (!fs.existsSync("channel.db")) {
   fs["writeFileSync"]("./channel.db", '{"channel": {}}');
@@ -64,52 +75,3 @@ app.get("/favicon.ico", (req, res) => {
 import { handler } from "./build/handler.js";
 
 app.use(handler);
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(
-    "==================================================================="
-  );
-  console.log(
-    chalk.green("THIS SERVER IS 100% FREE. PLEASE DON'T PAY ANYONE.")
-  );
-  console.log(
-    chalk.green(
-      "STRICT ACTION WILL BE TAKEN AGAINST THOSE WHO ARE SELLING THIS."
-    )
-  );
-  console.log(
-    chalk.green(
-      "IF ANYONE TRYING TO SELL IT, PLEASE REPORT IT ON OUR DISCORD SERVER\nhttps://discord.gg/suyzkCQKhC"
-    )
-  );
-  console.log(
-    chalk.green("Please Stare Repo https://github.com/dhruv-2015/JIOTVServer")
-  );
-  console.log(chalk.green("VERSION 2.8.6"));
-  console.log(
-    chalk.green(
-      "980+ CHANNELS AVAILABLE. MORE FEATURES WILL COME SOON. STAY TUNED"
-    )
-  );
-  console.log(
-    "==================================================================="
-  );
-  console.log(
-    chalk.red(
-      "       __ ____ ____     ______ _    __ \n      / //  _// __ \\   /_  __/| |  / / \n __  / / / / / / / /    / /   | | / /  \n/ /_/ /_/ / / /_/ /    / /    | |/ /   \n\\____//___/ \\____/    /_/     |___/    \n                                "
-    )
-  );
-  console.log(
-    "==================================================================="
-  );
-  console.log(`TV server is running on port ${PORT}`);
-  console.log(
-    `Please open http://localhost:${PORT}/login from the host to login and get playlist if running server for the first time`
-  );
-  console.log(
-    `Note: If hosted in a website, you may want to use the domain or its IP`
-  );
-  // console.log(chalk.red("need to login every 24 hours even if you are already logged in"));
-  console.log("If facing any errors, please login from portal again");
-  console.log("you can use server m3u8 links in other websites and apps");
-});
